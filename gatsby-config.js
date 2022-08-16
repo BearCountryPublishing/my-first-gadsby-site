a992cde3d3e9b915d298785fd36ffcb22db52f10
Mark Caldwell <markcaldwell@Marks-Air.local.lan>
2022-08-16 13:05:56 -0800

    Finished tutorial part 3. Added image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     title: `My First Gadsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Bear Country Publishing">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt='Circuit Map of US Courts'
+        src='../images/CircuitMap.svg'
+      />
     </Layout>
   )
 }
