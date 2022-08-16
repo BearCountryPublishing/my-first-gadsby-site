9ee2081253ee5f24bb12f25cc6bdf06f2fe62982
Mark Caldwell <markcaldwell@Marks-Air.local.lan>
2022-08-16 03:59:58 -0800

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About">
+      <p>I'm building this Gatsby & React site from scratch!</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
